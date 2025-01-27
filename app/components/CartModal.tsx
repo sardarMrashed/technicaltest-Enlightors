5f9e1171baea6fb8b95071c764b1a2d9a170da81
Sardar1 <sardarmohamad96@gmail.com>
2025-01-27 21:21:00 +0300

    design  in files  cart for buy thing

--- app/components/CartModal.tsx
+++ app/components/CartModal.tsx
@@ -1,7 +1,7 @@
 'use client'
 const CartModal = () => {
   return (
-    <div>CartModal</div>
+    <div className="absolute p-4 rounded-md shadow-[0px_3px_10px_rgba(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-4 ">CartModal</div>
  
   )
 }
