a832a66fc2870bd16003b5b8ffa38b086c9685d9
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 14:26:54 +0300

    feat: enhance ListPage layout with image integration and structural adjustments

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,10 +1,17 @@
+import Image from 'next/image'
+
+
 const ListPage = () => {
   return (
     <div className="px-4 Md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
-      <div>List</div>
-      {/* Add your code here */}
-      
-      page !
+   <div className="1">
+    <div></div>
+      <div className="3">
+    <Image src="http://localhost:3000/1.png" alt="" width={200} height={200} />
+     
+    </div>
+   </div>
+!
     </div>
   )
 }
