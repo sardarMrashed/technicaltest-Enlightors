07d8a532f38d4063e57ce13e48319bb6d303c2dd
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 14:24:09 +0300

    working on files ub the listpage

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,6 +1,9 @@
 const ListPage = () => {
   return (
     <div className="px-4 Md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
+      <div>List</div>
+      {/* Add your code here */}
+      
       page !
     </div>
   )
