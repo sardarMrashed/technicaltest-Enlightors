1404dc35ddb8c54160054f5611d18a370f5b0067
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 16:00:25 +0300

    feat: enhance Filter component with additional input fields and improved select options

--- app/components/Filter.tsx
+++ app/components/Filter.tsx
@@ -2,29 +2,32 @@ const Filter = () => {
   return (
     <div className="mt-12 flex justify-between">
       <div className="flex gap-6 flex-wrap">
+        <select 
+        name="type" 
+        id=""
+         className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200">
+          <option>Type</option>
+          <option value="Physical">Physical</option>
+          <option value="Digital">Digital</option>
+        </select>
+        <input type="text" name="min"  placeholder="min Price."/>
+         type="text"
+          placeholder="min Price."
+           className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200" />
         <select name="type" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200">
           <option value="all">Type</option>
           <option value="Physical">Physical</option>
           <option value="Digital">Digital</option>
-          <option value="kids">Kids</option>
         </select>
         <select name="type" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200">
           <option value="all">Type</option>
           <option value="Physical">Physical</option>
           <option value="Digital">Digital</option>
-          <option value="kids">Kids</option>
         </select>
         <select name="type" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200">
           <option value="all">Type</option>
           <option value="Physical">Physical</option>
           <option value="Digital">Digital</option>
-          <option value="kids">Kids</option>
-        </select>
-        <select name="type" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200">
-          <option value="all">Type</option>
-          <option value="Physical">Physical</option>
-          <option value="Digital">Digital</option>
-          <option value="kids">Kids</option>
         </select>
         </div>
       </div>
