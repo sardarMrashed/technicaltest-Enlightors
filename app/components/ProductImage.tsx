c2b8de45466bc11ab52f5395e9afce8f4cb9f444
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 19:20:18 +0300

    feat: add ProductImage component with image and placeholder for product name

--- app/components/ProductImage.tsx
+++ app/components/ProductImage.tsx
@@ -2,6 +2,13 @@
 const ProductImage = () => {
   return (
     <div>
+      <div className="h-[500px]">
+        {/* img */}
+        <img src="product_image.jpg" alt="product_image" width={200} height={200} />
+        {/* Product Name */}
+      </div>
+
+
       
     </div>
   )
