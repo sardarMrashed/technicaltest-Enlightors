b5af078f6ec35de14dd2f60d17fee1ad99af2226
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 15:18:56 +0300

    add to the components

--- app/components/Filter.tsx
+++ app/components/Filter.tsx
@@ -1,8 +1,15 @@
 const Filter = () => {
   return (
-    <div>
-      
-    </div>
+    <div className="mt-12 flex justify-between">
+      <div className="flex gap-6 flex-wrap">
+        <select name="type" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200">
+          <option value="all">Type</option>
+          <option value="Physical">Physical</option>
+          <option value="Digital">Digital</option>
+          <option value="kids">Kids</option>
+        </select>
+        </div>
+      </div>
   )
 }
 
