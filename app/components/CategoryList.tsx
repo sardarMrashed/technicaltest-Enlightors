b0fec6a64db557f1b5a0cc2b9ca535e8eae67014
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 10:54:05 +0300

    feat: add image and category name to CategoryList component

--- app/components/CategoryList.tsx
+++ app/components/CategoryList.tsx
@@ -6,13 +6,14 @@ const CategoryList = () => {
       <div className="flex gap-4 md:gap-8">
         <Link href='/list?cat=test'>
         <div className='relative bg-slate-100 w-full h-98'>
-       <Image src='' 
+       <Image src='https://cdn.pixabay.com/photo/2016/11/22/22/39/golf-1850984_1280.jpg' 
        alt='food' 
        sizes='20vw'
        fill
        className='object-cover' />
           <p className='text-center'>Food</p>
         </div>
+        <h1 className='mt-8 font-light text-cl tracking-wide'>Categories Name</h1>
         </Link>
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       </div>
       <div className="mt-25 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
         <h1 className="text-2xl font-semibold tracking-wide">New Arrivals</h1>
-      <ProductList />
+
       </div>
 
 
