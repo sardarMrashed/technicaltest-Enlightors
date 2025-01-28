90428b0f22657d41fecaee9476754a8f925c02ed
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 10:49:50 +0300

    fix: correct component name and enhance CategoryList with image and link functionality

--- app/components/CategoryList.tsx
+++ app/components/CategoryList.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
-const CaegoryList = () => {
+import Image from 'next/image'
+import Link from 'next/link'
+const CategoryList = () => {
   return (
     <div className='px-4 overflow-x-scroll scrollbar-hide'>
-      
+      <div className="flex gap-4 md:gap-8">
+        <Link href='/list?cat=test'>
+        <div className='relative bg-slate-100 w-full h-98'>
+       <Image src='' 
+       alt='food' 
+       sizes='20vw'
+       fill
+       className='object-cover' />
+          <p className='text-center'>Food</p>
+        </div>
+        </Link>
+
+
+      </div>
     </div>
   )
 }
 
-export default categoryList
+export default CategoryList
