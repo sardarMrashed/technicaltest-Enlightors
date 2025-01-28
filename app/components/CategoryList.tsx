85abd771f7770bb91d1779b670d3674d9042a98b
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 11:09:26 +0300

    feat: enhance CategoryList component with multiple image and link entries

--- app/components/CategoryList.tsx
+++ app/components/CategoryList.tsx
@@ -17,6 +17,66 @@ const CategoryList = () => {
           </div>
                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
         </Link>
+        <Link href="/list?cat=test" className='className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"'>
+          <div className="relative bg-slate-100 w-60 h-96 rounded-lg overflow-hidden shadow-lg">
+            <Image 
+              src="https://images.pexels.com/photos/12102576/pexels-photo-12102576.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+              alt="Food category" 
+              fill 
+              sizes="20vw" 
+              className="object-cover" 
+            />
+          </div>
+                   <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+        </Link>
+        <Link href="/list?cat=test" className='className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"'>
+          <div className="relative bg-slate-100 w-60 h-96 rounded-lg overflow-hidden shadow-lg">
+            <Image 
+              src="https://images.pexels.com/photos/12102576/pexels-photo-12102576.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+              alt="Food category" 
+              fill 
+              sizes="20vw" 
+              className="object-cover" 
+            />
+          </div>
+                   <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+        </Link>
+        <Link href="/list?cat=test" className='className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"'>
+          <div className="relative bg-slate-100 w-60 h-96 rounded-lg overflow-hidden shadow-lg">
+            <Image 
+              src="https://images.pexels.com/photos/12102576/pexels-photo-12102576.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+              alt="Food category" 
+              fill 
+              sizes="20vw" 
+              className="object-cover" 
+            />
+          </div>
+                   <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+        </Link>
+        <Link href="/list?cat=test" className='className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"'>
+          <div className="relative bg-slate-100 w-60 h-96 rounded-lg overflow-hidden shadow-lg">
+            <Image 
+              src="https://images.pexels.com/photos/12102576/pexels-photo-12102576.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+              alt="Food category" 
+              fill 
+              sizes="20vw" 
+              className="object-cover" 
+            />
+          </div>
+                   <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+        </Link>
+        <Link href="/list?cat=test" className='className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"'>
+          <div className="relative bg-slate-100 w-60 h-96 rounded-lg overflow-hidden shadow-lg">
+            <Image 
+              src="https://images.pexels.com/photos/12102576/pexels-photo-12102576.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+              alt="Food category" 
+              fill 
+              sizes="20vw" 
+              className="object-cover" 
+            />
+          </div>
+                   <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
+        </Link>
       </div>
     </div>
   )
