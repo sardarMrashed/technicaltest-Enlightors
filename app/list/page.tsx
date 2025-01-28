f94c767c2999b5794101fa3a80efa498d795a3ec
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 14:31:17 +0300

    feat: update ListPage layout with improved image handling and responsive design

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -5,9 +5,12 @@ const ListPage = () => {
   return (
     <div className="px-4 Md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
    <div className="1">
-    <div></div>
-      <div className="3">
-    <Image src="http://localhost:3000/1.png" alt="" width={200} height={200} />
+    <div className="w-2/3"></div>
+      <div className="relative w-1/3">
+    <Image src="woman.png" alt="" 
+    fill 
+    className='object-contain'
+     />
      
     </div>
    </div>
