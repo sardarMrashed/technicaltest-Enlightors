d460b74f87f0cc85a3166ea2430cf32ddbec59d6
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 08:48:53 +0300

    add heading for featured products in home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
   return (
     <div>
       <Slider />
-      <div className="mt-25">
+      <div className="mt-25 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+        <h1 className="text-4xl font-semibold tracking-wide">Featured Products</h1>
+      
       <ProductList />
       </div>
 
