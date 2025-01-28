6bd68a592a59694085a47e3c6f2e8c70afa4d0b5
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 17:07:57 +0300

    feat: update SinglePage layout to include text section with improved styling

--- app/[slug]/SinglePage.tsx
+++ app/[slug]/SinglePage.tsx
@@ -7,8 +7,8 @@ const SinglePage = () => {
        <ProductImage />
       </div>
       {/* text */}
-      <div className="w-full lg:w-1/2"></div>
-    </div>
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">Text</div>
+      </div>
   )
 }
 
