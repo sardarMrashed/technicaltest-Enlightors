c88881d661e840c9b8d8081cbfb159bf2d0ae21e
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 15:08:14 +0300

    feat: update ListPage component with enhanced promotional section and improved styling

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -3,26 +3,24 @@ import Image from "next/image";
 
 
 const ListPage = () => {
-
-
-
-
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* CAMPAIGN */}
+
       <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64">
         <div className="w-2/3 flex flex-col items-center justify-center gap-8">
-          <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
-            Grab up to 50% off on
-            <br /> Selected Products
-          </h1>
-          <button className="rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm">
-            Buy Now
-          </button>
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-semibold text-gray-800 leading-tight tracking-tight sm:w-3/4">
+  Grab up to <span className="text-lama">60% off</span> on
+  <br /> Selected Products
+</h1>
+<button className="rounded-full bg-lama text-white py-3 px-8 text-base font-semibold shadow-lg hover:bg-lama-dark transition-all duration-300 ease-in-out">
+  Buy Now
+</button>
         </div>
-        <div className="relative w-1/3">
+        
+    <div className="relative w-1/3">
           <Image src="/woman.png" alt="" fill className="object-contain" />
-        </div>
+        </div> 
       </div>
       {/* FILTER */}
    
