22e72a54d07a93e885f2e8820e23cfb8fb882b40
Sardar1 <sardarmohamad96@gmail.com>
2025-01-28 08:50:49 +0300

    refactor ProductList component to improve image rendering and layout consistency

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -6,20 +6,21 @@ const ProductList = () => {
   return (
     <div className='flex gap-x-8 gap-y-16 justify-between flex-wrap re'>
         <Link href="/test" className="relative w-full h-96">
-              <Image src="https://cdn.pixabay.com/photo/2016/11/22/22/39/golf-1850984_1280.jpg" alt="Product 1" fill sizes="25vw" className="object-cover rounded-lg shadow-md" />
+        
+              src="https://cdn.pixabay.com/photo/2016/11/22/22/39/golf-1850984_1280.jpg" 
+              alt="Product 1"
+               fill sizes="25vw" 
+               className="object-cover rounded-lg shadow-md" 
+               />
+                   <Image 
+              src="https://cdn.pixabay.com/photo/2016/11/22/22/39/golf-1850984_1280.jpg" 
+              alt="Product 1"
+               fill sizes="25vw" 
+               className="object-cover rounded-lg shadow-md" 
+               />
+
         </Link>
-        <Link href="/product/2">
-            <div className='w-1/3 h-96 bg-gray-100 flex flex-col justify-end p-4'>
-                <h2 className='text-2xl font-bold'>Product 2</h2>
-                <p className='text-lg font-light'>$100</p>
-            </div>
-        </Link>
-        <Link href="/product/3">
-            <div className='w-1/3 h-96 bg-gray-100 flex flex-col justify-end p-4'>
-                <h2 className='text-2xl font-bold'>Product 3</h2>
-                <p className='text-lg font-light'>$100</p>
-            </div>
-        </Link>
+        
     </div>
   )
 }
